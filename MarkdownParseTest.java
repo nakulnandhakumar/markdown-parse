bbc2b31c961128922050ad58dc19eb833ddd8adc
nakulnandhakumar <nakul@nandhakumar.net>
2022-01-26 15:33:46 -0800

    Test File added and JUnit imported

---
+++ MarkdownParseTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+// imports the JUnit classes and methods
+
+public class MarkdownParseTest {
+    @Test 
+    // annotation defines the method below as a JUnit test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+        // assertEquals checks if the value of the first paramater is
+        // equivalent to the value of the second parameter
+    }
+}
