d07f1a4f816282017ef4add445053421551089ce
nakulnandhakumar <nakul@nandhakumar.net>
2022-02-02 16:32:40 -0800

    added a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -30,6 +30,7 @@ public class MarkdownParse {
             // Only adds text inside parentheses if it is actually a link (parentheses must be right after closed brackets)
 
             //toReturn.add(markdown.substring(openParen + 1, closeParen));
+            // this is a changed line
             currentIndex = closeParen + 1;
         }
         return toReturn;
